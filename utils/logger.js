978bff9eeeb315f034cb9620dc92c72a1c73e8e5
CyberSecurityTower <159848727+CyberSecurityTower@users.noreply.github.com>
2025-11-05 22:51:45 +0100

    Implement logger utility with multiple log levels

--- utils/logger.js
+++ utils/logger.js
@@ -1,1 +1,17 @@
 
+// utils/logger.js
+'use strict';
+
+const iso = () => new Date().toISOString();
+
+const logger = {
+  info: (msg, ...args) => console.log(`\x1b[36m${iso()} [INFO] ${msg}\x1b[0m`, ...args),
+  success: (msg, ...args) => console.log(`\x1b[32m${iso()} [SUCCESS] ${msg}\x1b[0m`, ...args),
+  warn: (msg, ...args) => console.warn(`\x1b[33m${iso()} [WARN] ${msg}\x1b[0m`, ...args),
+  error: (msg, ...args) => console.error(`\x1b[31m${iso()} [ERROR] ${msg}\x1b[0m`, ...args),
+  log: (msg, ...args) => console.log(`${iso()} [LOG] ${msg}`, ...args),
+  // Request-specific logger (will be assigned in middleware)
+  req: (requestId, ...args) => console.log(iso(), `[req:${requestId}]`, ...args)
+};
+
+module.exports = logger;
