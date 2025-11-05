9b2dbda4d86db584bd3708ecad4a53f51d15ccb3
CyberSecurityTower <159848727+CyberSecurityTower@users.noreply.github.com>
2025-11-05 22:54:05 +0100

    Add requestId middleware for tracking requests

--- middleware/requestId.js
+++ middleware/requestId.js
@@ -1,1 +1,16 @@
 
+
+// middleware/requestId.js
+'use strict';
+
+const crypto = require('crypto');
+const logger = require('../utils/logger');
+
+function requestIdMiddleware(req, res, next) {
+  req.requestId = req.headers['x-request-id'] || crypto.randomUUID();
+  res.setHeader('X-Request-Id', req.requestId);
+  req.log = (...args) => logger.req(req.requestId, ...args); // Use the unified logger
+  next();
+}
+
+module.exports = requestIdMiddleware;
