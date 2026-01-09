6b266d7a448e8ca67150f78316c92bbaa279c361
CyberSecurityTower <159848727+CyberSecurityTower@users.noreply.github.com>
2026-01-10 00:59:50 +0100

    Create upload.js

---
+++ middleware/upload.js
@@ -0,0 +1,21 @@
+// middleware/upload.js
+const multer = require('multer');
+const path = require('path');
+
+// تخزين مؤقت في مجلد 'uploads'
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/') // تأكد من إنشاء هذا المجلد في الجذر
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ 
+    storage: storage,
+    limits: { fileSize: 10 * 1024 * 1024 } // 10MB limit per file
+});
+
+module.exports = upload;
