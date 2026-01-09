7190533395a674b48042db0a25f64d4d23407f20
CyberSecurityTower <159848727+CyberSecurityTower@users.noreply.github.com>
2026-01-09 03:11:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const logger = require('./utils/logger');
 
 const app = express();
 
+app.use(express.json({ limit: '50mb' })); 
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 // --- إعدادات Middleware (مرة واحدة فقط) ---
 
 // 1. CORS & Security Headers
