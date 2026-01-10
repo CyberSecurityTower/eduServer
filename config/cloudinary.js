911ad579bdb365798c027685f35faa64ee4e51d8
CyberSecurityTower <159848727+CyberSecurityTower@users.noreply.github.com>
2026-01-10 01:13:22 +0100

    Update cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,6 +1,9 @@
 // config/cloudinary.js
+'use strict';
+
 const cloudinary = require('cloudinary').v2;
 
+// هنا الكود يقرا المتغيرات اللي حطيناهم في Render
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
